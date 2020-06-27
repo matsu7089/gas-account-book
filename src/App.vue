26da3015e0c4db3f9e4bd49bc67884585050de9f
matsu7089 <32781959+matsu7089@users.noreply.github.com>
2020-06-27 23:59:29 +0900

    update: Vuex を利用するように更新

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <!-- ツールバー -->
     <v-app-bar app color="green" dark>
       <!-- タイトル -->
-      <v-toolbar-title>GAS 家計簿</v-toolbar-title>
+      <v-toolbar-title>{{ appName }}</v-toolbar-title>
       <v-spacer></v-spacer>
       <!-- テーブルアイコンのボタン -->
       <v-btn icon to="/">
@@ -25,7 +25,18 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
-  name: 'App'
+  name: 'App',
+
+  computed: mapState({
+    appName: state => state.settings.appName
+  }),
+
+  // Appインスタンス生成前に一度だけ実行されます
+  beforeCreate () {
+    this.$store.dispatch('loadSettings')
+  }
 }
 </script>
