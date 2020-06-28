d280750d338345837b5fe16b0612256b81882361
matsu7089 <32781959+matsu7089@users.noreply.github.com>
2020-06-28 13:28:47 +0900

    update: 家計簿データの操作を実装

--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,9 @@ Vue.use(Vuex)
  * Vuexの状態
  */
 const state = {
+  /** 家計簿データ */
+  abData: {},
+
   /** 設定 */
   settings: {
     appName: 'GAS 家計簿',
@@ -24,6 +27,38 @@ const state = {
  * ActionsからStateを更新するときに呼ばれます
  */
 const mutations = {
+  /** 指定年月の家計簿データをセットします */
+  setAbData (state, { yearMonth, list }) {
+    state.abData[yearMonth] = list
+  },
+
+  /** データを追加します */
+  addAbData (state, { item }) {
+    const yearMonth = item.date.slice(0, 7)
+    const list = state.abData[yearMonth]
+    if (list) {
+      list.push(item)
+    }
+  },
+
+  /** 指定年月のデータを更新します */
+  updateAbData (state, { yearMonth, item }) {
+    const list = state.abData[yearMonth]
+    if (list) {
+      const index = list.findIndex(v => v.id === item.id)
+      list.splice(index, 1, item)
+    }
+  },
+
+  /** 指定年月&IDのデータを削除します */
+  deleteAbData (state, { yearMonth, id }) {
+    const list = state.abData[yearMonth]
+    if (list) {
+      const index = list.findIndex(v => v.id === id)
+      list.splice(index, 1)
+    }
+  },
+
   /** 設定を保存します */
   saveSettings (state, { settings }) {
     state.settings = { ...settings }
@@ -47,6 +82,40 @@ const mutations = {
  * 画面から呼ばれ、Mutationをコミットします
  */
 const actions = {
+  /** 指定年月の家計簿データを取得します */
+  fetchAbData ({ commit }, { yearMonth }) {
+    // サンプルデータを初期値として入れる
+    const list = [
+      { id: 'a34109ed', date: `${yearMonth}-01`, title: '支出サンプル', category: '買い物', tags: 'タグ1', income: null, outgo: 2000, memo: 'メモ' },
+      { id: '7c8fa764', date: `${yearMonth}-02`, title: '収入サンプル', category: '給料', tags:'タグ1,タグ2', income: 2000, outgo: null, memo: 'メモ' }
+    ]
+    commit('setAbData', { yearMonth, list })
+  },
+
+  /** データを追加します */
+  addAbData ({ commit }, { item }) {
+    commit('addAbData', { item })
+  },
+
+  /** データを更新します */
+  updateAbData ({ commit }, { beforeYM, item }) {
+    const yearMonth = item.date.slice(0, 7)
+    if (yearMonth === beforeYM) {
+      commit('updateAbData', { yearMonth, item })
+      return
+    }
+    const id = item.id
+    commit('deleteAbData', { yearMonth: beforeYM, id })
+    commit('addAbData', { item })
+  },
+
+  /** データを削除します */
+  deleteAbData ({ commit }, { item }) {
+    const yearMonth = item.date.slice(0, 7)
+    const id = item.id
+    commit('deleteAbData', { yearMonth, id })
+  },
+
   /** 設定を保存します */
   saveSettings ({ commit }, { settings }) {
     commit('saveSettings', { settings })
