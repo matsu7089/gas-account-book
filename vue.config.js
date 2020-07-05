74c70c20fb3b3e89b79d8c7b644a1906c1bb935d
matsu7089 <32781959+matsu7089@users.noreply.github.com>
2020-07-05 16:41:47 +0900

    update: production時の設定を追加

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  publicPath: isProd ? '/gas-account-book' : '/',
+  outputDir: 'docs',
+  filenameHashing: false,
+  productionSourceMap: false
 }
